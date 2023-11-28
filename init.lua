ad01eaa2cfe160fa347b341200a803b0d2d44838
mrx7355608 <mrx7355608@gmail.com>
2023-11-28 21:35:45 +0500

    luasnip setup (as it was not working)

--- init.lua
+++ init.lua
@@ -106,8 +106,8 @@ require("lazy").setup({
 		"L3MON4D3/LuaSnip",
 		-- follow latest release.
 		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-		-- install jsregexp (optional!).
 		build = "make install_jsregexp",
+		dependencies = { "rafamadriz/friendly-snippets" },
 	},
 
 	{
@@ -157,3 +157,4 @@ require("plugins.mason")
 require("plugins.lspsaga")
 require("plugins.cmp")
 require("plugins.lualine")
+require("plugins.luasnip")
