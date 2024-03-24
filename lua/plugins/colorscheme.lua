15e39296c8cc23e83e61efd86ac4eb12f90ee9a7
mrx7355608 <mrx7355608@gmail.com>
2024-03-24 08:53:43 +0500

    changed colorscheme to tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,1 @@
-require("ayu").setup({
-	mirage = false,
-	overrides = {
-		Normal = { bg = "None" },
-		ColorColumn = { bg = "None" },
-		SignColumn = { bg = "None" },
-		Folded = { bg = "None" },
-		FoldColumn = { bg = "None" },
-		CursorLine = { bg = "None" },
-		CursorColumn = { bg = "None" },
-		WhichKeyFloat = { bg = "None" },
-		VertSplit = { bg = "None" },
-	},
-})
-
-vim.cmd.colorscheme("ayu")
+vim.cmd.colorscheme("catppuccin-mocha")
